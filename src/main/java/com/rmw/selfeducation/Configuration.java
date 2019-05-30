465c368d8efeb8edef477e5c378512b0b8fc25b8
Nadezhda Dubrovskaya <n.dubrovskaya@itransition.com>
2019-05-30 17:19:03 +0300

    Changed the map - increased the matrix and decreased the scale, rearranged the walls

--- src/main/java/com/rmw/selfeducation/Configuration.java
+++ src/main/java/com/rmw/selfeducation/Configuration.java
@@ -8,15 +8,15 @@ class Configuration {
      */
     static final boolean IS_AI_MODE = true;
 
-    static final int ROWS = 10;
-    static final int COLS = 15;
+    static final int ROWS = 15;
+    static final int COLS = 25;
 
     /**
      * Sets the correlation between coordinates (pixels) and the array of tiles.
      * For example, if the scale is set to 50 then it means each tile will be a square of 50 "pixels".
      * Meaning that a tile array 10x15 in size will require a screen of 500x750 px
      */
-    static final int SCALE = 50;
+    static final int SCALE = 30;
 
     private Configuration() {
     }
